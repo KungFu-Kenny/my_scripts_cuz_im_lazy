91cbbc62a6e0af7a96887e32bab2df3f5b5ea8f5
K.DOT <32544469+KungFu-Kenny@users.noreply.github.com>
2018-12-03 10:58:49 +0100

    Update mariadb.sh
    added auto yes

--- mariadb.sh
+++ mariadb.sh
@@ -2,8 +2,8 @@
 
 #mariadb UBUNTU 18.04
 
-sudo apt-get install software-properties-common
+sudo apt -y install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.igh.cnrs.fr/pub/mariadb/repo/10.3/ubuntu bionic main'
-sudo apt update
-sudo apt install mariadb-server
+sudo apt -y update
+sudo apt -y install mariadb-server
